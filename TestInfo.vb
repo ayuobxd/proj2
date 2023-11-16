c2b64a9259e66fb68069c6509c95a0248ffd3c0c
ayuobxd <ayuobsmaili2017@gmail.com>
2023-11-16 16:42:21 +0100

    hello

--- TestInfo.vb
+++ TestInfo.vb
@@ -3,5 +3,11 @@
         Call CenterToScreen()
         Me.FormBorderStyle = FormBorderStyle.FixedDialog
         Me.MaximizeBox = MaximizeBox.Equals(0)
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(TextBox1.Text)
     End Sub
 End Class
